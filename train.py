3cdc4a9f091bd7e818590da62041aec6474b2ea2
flashhhhh <111177346+flashhhhh@users.noreply.github.com>
2024-05-18 19:05:36 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO('best2.pt')
-results = model.train(data='dataset/data.yaml', epochs=5, batch=8, imgsz=640, project='runs/train', name='Sino-nom')
+results = model.train(data='dataset/data.yaml', epochs=100, batch=8, imgsz=640, project='runs/train', name='Sino-nom')
